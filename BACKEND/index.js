bda58e9cf8e02f7f16753054d936ff72174a78b0
PedroAT79 <plat1505@gmail.com>
2023-11-09 19:15:27 +0100

    configuracion cors

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,17 +1,42 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors';
 import conectarDB from './config/db.js';
+import clienteRoutes from './routes/clienteRoutes.js'
+import presupuestoRoutes from './routes/presupuestoRoutes.js'
+import cestaRoutes from './routes/cestaRoutes.js'
+
+
 
 
 const app = express();
-dotenv.configDotenv();
-
-
-conectarDB();
-
-const PORT = process.env.PORT || 4000;
 app.use(express.json());
 
+dotenv.configDotenv();
+
+const dominiosPermitidos = ['http://127.0.0.1:5500/'];
+const corsOptions = {
+    origin: function (origin, callback) {
+       if(dominiosPermitidos.indexOf(origin) !==-1){
+          callback(null, true);
+       } else {
+          callback(new Error('No permitido por Cors'));
+       }
+       
+    }
+ }
+
+ app.use(cors(dominiosPermitidos));
+ 
+conectarDB();
+
+
+
+app.use('/webdev/clientes', clienteRoutes);
+app.use('/webdev/cestas', cestaRoutes);
+app.use('/webdev/presupuestos', presupuestoRoutes);
+
+const PORT = process.env.PORT || 4500;
 app.listen(PORT, ()=>{
     console.log(`Servidor funcionando en el puerto ${PORT}`);
 })
