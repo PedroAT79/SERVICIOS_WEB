3e757013015a442f88ccd27512d5e833dd55a613
PedroAT79 <plat1505@gmail.com>
2023-11-09 19:16:23 +0100

    eliminacion de elemento deprecados de db.js

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -4,8 +4,9 @@ const conectarDB = async () =>{
     try {
         const db = await mongoose.connect(process.env.MONGO_URI,
         {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+            autoIndex: false,
+            //useNewUrlParser: true,
+            //useUnifiedTopology: true
         });
     
         const url = `${db.connection.host}:${db.connection.port}`;
